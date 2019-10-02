4e3034dd93982d3af3584224743c29fad1cd53a4
Derrick Smith <derricksmith01@msn.com>
2019-10-01 23:46:08 -0600

    Update __init__.py

--- src/algorithms/__init__.py
+++ src/algorithms/__init__.py
@@ -1,5 +1,6 @@
 from algorithms.top_movers_no_day_trades import *
 from algorithms.no_day_trades import *
+from algorithms.no_day_trades_ds import *
 from algorithms.short_intensive import *
 from algorithms.__algorithm import *
 from algorithms.__skeleton import *
